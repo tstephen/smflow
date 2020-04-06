5b8449590aa3def9cc2d48cec4bad893d6c10472
Tim Stephenson <tim@knowprocess.com>
2020-04-06 22:30:54 +0100

    scripts to filter tasks and instances and del instance

--- src/scripts/get-instances.sh
+++ src/scripts/get-instances.sh
@@ -1,1 +1,7 @@
-curl -v -H 'Accept:application/json' -u $FLOWABLE_USR:$FLOWABLE_PWD $FLOWABLE_URL/runtime/process-instances
+if [ -z "$1" ] # use -z to get size of param 1
+then
+  curl -v -H 'Accept:application/json' -u $FLOWABLE_USR:$FLOWABLE_PWD $FLOWABLE_URL/runtime/process-instances
+else
+  echo 'Query filtered by '$1
+  curl -v -H 'Accept:application/json' -u $FLOWABLE_USR:$FLOWABLE_PWD $FLOWABLE_URL/runtime/process-instances?$1
+fi
--- src/scripts/get-tasks.sh
+++ src/scripts/get-tasks.sh
@@ -1,1 +1,8 @@
-curl -v -H 'Accept:application/json' -u $FLOWABLE_USR:$FLOWABLE_PWD $FLOWABLE_URL/runtime/tasks
+if [ -z "$1" ] # use -z to get size of param 1
+then
+  curl -v -H 'Accept:application/json' -u $FLOWABLE_USR:$FLOWABLE_PWD $FLOWABLE_URL/runtime/tasks
+else
+  echo 'Query filtered by '$1
+  curl -v -H 'Accept:application/json' -u $FLOWABLE_USR:$FLOWABLE_PWD $FLOWABLE_URL/runtime/tasks?$1
+fi
+
